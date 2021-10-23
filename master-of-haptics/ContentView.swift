f6b3ab3221cc6a940f8a8619085ab8a19cffee9c
Marcus Ziadé <marcus.ziade@ouraring.com>
2021-10-23 14:02:12 +0300

    Remove redundant UI

--- master-of-haptics/ContentView.swift
+++ master-of-haptics/ContentView.swift
@@ -5,26 +5,21 @@
 //  Created by Marcus Ziadé on 23.10.2021.
 //
 
-import Combine
 import SwiftUI
 
 struct HapticShowroomView: View {
 
-    @State private var selectionIntensity: Float = 0.5
     @State private var impactIntensity: Float = 0.5
-    @State private var notificationIntensity: Float = 0.5
 
     var body: some View {
         List {
             Section {
-                Slider(value: $selectionIntensity, in: 0...1)
-                Label(String(selectionIntensity), systemImage: "bolt.circle.fill")
-
                 Button { UISelectionFeedbackGenerator().selectionChanged() } label: {
                     Text("Selection").foregroundColor(.primary)
                 }
-            } header: { Text("Selection") }
-
+            } header: {
+                Text("Selection")
+            }
 
             Section {
                 Slider(value: $impactIntensity, in: 0...1)
@@ -33,20 +28,23 @@ struct HapticShowroomView: View {
                 ForEach(UIImpactFeedbackGenerator.FeedbackStyle.allCases, id: \.self) { style in
                     Button {
                         UIImpactFeedbackGenerator(style: style).impactOccurred(intensity: CGFloat(impactIntensity))
-                    } label: { Text(style.title).foregroundColor(.primary).fontWeight(style.font) }
+                    } label: {
+                        Text(style.title).foregroundColor(.primary).fontWeight(style.font)
+                    }
                 }
-            } header: { Text("Impact") }
+            } header: {
+                Text("Impact")
+            }
 
             Section {
-                Slider(value: $notificationIntensity, in: 0...1)
-                Label(String(notificationIntensity), systemImage: "bolt.circle.fill")
-
                 ForEach(UINotificationFeedbackGenerator.FeedbackType.allCases, id: \.self) { style in
                     Button { UINotificationFeedbackGenerator().notificationOccurred(style) } label: {
                         Text(style.title).foregroundColor(style.color)
                     }
                 }
-            } header: { Text("Notifications") }
+            } header: {
+                Text("Notifications")
+            }
         }
     }
 }
