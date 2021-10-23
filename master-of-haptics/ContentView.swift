c5201424920ea1c94f7427fa52e499a0afde9962
Marcus Ziadé <marcus.ziade@ouraring.com>
2021-10-23 11:09:21 +0300

    Initial Commit

---
+++ master-of-haptics/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  master-of-haptics
+//
+//  Created by Marcus Ziadé on 23.10.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ master-of-haptics/master_of_hapticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  master_of_hapticsApp.swift
+//  master-of-haptics
+//
+//  Created by Marcus Ziadé on 23.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct master_of_hapticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
